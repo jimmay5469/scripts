0426cefd3b383c5dbc47a6830e7daa2d319ed5dd
jimmay5469 <jimmay5469@gmail.com>
2018-01-03 11:38:47 -0500

    Normalize src url

--- web-image-downloader.js
+++ web-image-downloader.js
@@ -1,14 +1,14 @@
 const request = require('request');
 const cheerio = require('cheerio');
-var Jimp = require("jimp");
+const Jimp = require("jimp");
+const normalizeUrl = require('normalize-url');
 
-
-request(process.argv[2], function (error, response, html) {
+request(normalizeUrl(process.argv[2]), function (error, response, html) {
   if (!error && response.statusCode == 200) {
 		const $ = cheerio.load(html);
     const imageUrl = $(process.argv[3]).attr('src');
 
-    Jimp.read(imageUrl, function(err, image) {
+    Jimp.read(normalizeUrl(imageUrl), function(err, image) {
       image.write(process.argv[4]);
     });
   }
